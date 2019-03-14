6073a6f7ad3a23881c094c03556e1cc40ee2d1ca
pavan-pan <pavan.pandappa@gmail.com>
2019-03-14 19:59:56 +0100

    added education in CV parts

--- CV_parts.cpp
+++ CV_parts.cpp
@@ -1,6 +1,7 @@
 //CV_parts.cpp
 #include "CV_parts.h"
 #include <iostream>
+
 void CV_parts::add_work_experience(Position pos)
 {
   Work_experience.push_back(pos);  
@@ -33,4 +34,38 @@ void CV_parts::print_work_experience()
        std::cout << *it1 << std::endl;
     }
   }
+}
+
+void CV_parts::add_education_qual (Education_qual edu)
+{
+	Education.push_back(edu);
+}
+
+int CV_parts::delete_education_qual()
+{
+  Education.clear();
+  if (Education.size() == 0)
+  {
+    return 0; //Delete successfull
+  }
+  else
+  {
+    return 1; //Delete unsuccessfull
+  }
+}
+
+void CV_parts::print_education_qual()
+{
+  /*for (std::vector<Position>::iterator it=Education.begin(); it!=Education.end(); it++)
+  {
+    std::cout << "position from date : " << it->get_from_date() << std::endl;
+    std::cout << "position to date : " << it->get_to_date() << std::endl;
+    std::cout << "position name : " << it->get_position_name() << std::endl;
+    std::cout  << "position responsibilities :" << std::endl;
+    std::vector<std::string> ex = it->get_responsibilities();
+    for (std::vector<std::string>::iterator it1=ex.begin(); it1!=ex.end(); it1++)
+    {
+       std::cout << *it1 << std::endl;
+    }
+  }*/
 }
--- CV_parts.h
+++ CV_parts.h
@@ -2,6 +2,7 @@
 #define CV_PARTS_H
 
 #include "position.h"
+#include "education_qual.h"
 #include <vector>
 #include <string>
 
@@ -9,11 +10,16 @@ class CV_parts
 {
   private:
      std::vector<Position> Work_experience;
+     std::vector<Education_qual> Education;
   
   public:
      void add_work_experience(Position pos);
      int delete_work_experience();
      void print_work_experience();
+
+     void add_education_qual (Education_qual edu);
+     int delete_education_qual();
+     void print_education_qual();
 
 };
 #endif
